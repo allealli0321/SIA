a03452884f05cfebfebb47c38dd598a168cb0589
allealli0321 <114583979+allealli0321@users.noreply.github.com>
2022-09-28 20:16:58 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Activity 1</title>
+</head>
+<body>
+<p>Ella Grace Facun</p> <br>
+<p>BSIT-4A</p> <br>
+<p>System Integration and Architecture</p> <br>
+</body>
+</html>
